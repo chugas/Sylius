218146a1d99ce58b79ed216891d39a74a82521ce
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-08-13 16:35:20 +0300

    add payum bundle.

--- src/Sylius/Bundle/CoreBundle/Model/Order.php
+++ src/Sylius/Bundle/CoreBundle/Model/Order.php
@@ -13,6 +13,8 @@ namespace Sylius\Bundle\CoreBundle\Model;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Payum\Model\DetailsAggregateInterface;
+use Payum\Model\DetailsAwareInterface;
 use Sylius\Bundle\AddressingBundle\Model\AddressInterface;
 use Sylius\Bundle\CartBundle\Model\Cart;
 use Sylius\Bundle\PaymentsBundle\Model\PaymentInterface;
@@ -23,7 +25,7 @@ use Sylius\Bundle\SalesBundle\Model\AdjustmentInterface;
  *
  * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
  */
-class Order extends Cart implements OrderInterface
+class Order extends Cart implements OrderInterface, DetailsAwareInterface, DetailsAggregateInterface
 {
     /**
      * User.
@@ -74,9 +76,10 @@ class Order extends Cart implements OrderInterface
      */
     protected $currency;
 
+    /**
+     * @var object
+     */
     protected $paymentDetails;
-
-    protected $paymentStatus;
 
     /**
      * Constructor.
@@ -429,5 +432,21 @@ class Order extends Cart implements OrderInterface
         }
 
         return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDetails()
+    {
+        return $this->paymentDetails;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setDetails($details)
+    {
+        $this->paymentDetails = $details;
     }
 }
